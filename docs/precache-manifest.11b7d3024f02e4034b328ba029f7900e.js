75ef8af3a0588a89b6ed65fe94abdb3f6302e3d0
Ryota Nakayama <casteira@m1-mbp.local>
2024-11-13 01:31:47 +0900

    changed post address

---
+++ docs/precache-manifest.11b7d3024f02e4034b328ba029f7900e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc4b7fbcb06758f3f1f37d2238bbd27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07f3aae60f37e65aa0eb",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "5e8da3b530d690605d9e",
+    "url": "/static/js/2.55edb292.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "07f3aae60f37e65aa0eb",
+    "url": "/static/js/main.e0d1bb82.chunk.js"
+  },
+  {
+    "revision": "ba90eaf63a0956376345",
+    "url": "/static/js/runtime~main.6f7dce58.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
